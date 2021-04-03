66b8bafbee0270f87b16f51a814ed347e4499040
faraamcode <abdulrasaqalagbede@gmail.com>
2021-04-03 14:25:42 +0100

    footer added and styled

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,10 +1,34 @@
-
+import {RiTruckLine} from 'react-icons/ri'
+import {FaAward} from 'react-icons/fa'
+import {CgSupport} from 'react-icons/cg'
+import {ImWhatsapp} from 'react-icons/im'
 
 function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+<footer>
+ <div className="first-section-footer">
+   <div className="section section-footer-logo">
+       <div className="single-footer-icon">
+        <RiTruckLine className="footer-icon"/>
+        <h4>fast delivery</h4>
+       </div>
+       <div className="single-footer-icon">
+        <FaAward className="footer-icon"/>
+        <h4>buyer protection</h4>
+       </div>
+       <div className="single-footer-icon">
+        <CgSupport className="footer-icon"/>
+        <h4>customer support</h4>
+       </div>
+   </div>
+ </div>
+ <div className="second-section-footer">
+     <h4>store details</h4>
+     <h3>target</h3>
+     <p>cham Tower, Plot 12 Nkruma rd, Kampala, ug</p>
+     <button className="footer-btn"><ImWhatsapp className="whatsapp"/>chat with us</button>
+ </div>
+</footer>
   );
 }
 
