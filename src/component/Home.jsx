eae33b9c1b93c822d6e532521d5f1540c7696357
faraamcode <abdulrasaqalagbede@gmail.com>
2021-04-04 09:08:24 +0100

    left side styled

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,8 +1,28 @@
+import {IoIosArrowForward} from "react-icons/io"
 function Home() {
     return (
-      <div>
-        <h1>Home</h1>
-      </div>
+ <main>
+     <section className="section section-main">
+         <div className="left-side">
+             <ul className="product-lists">
+                 <li className="product-item "><a href="#">Electronics(32)</a></li>
+                 <li className="product-item"><a className="active-item" href="#">Electronics(32)</a></li>
+                 <li className="product-item"><a href="#">Electronics(32)</a></li>
+                 <li className="product-item"><a href="#">Electronics(32)</a></li>
+                 <li className="product-item"><a href="#">Electronics(32)</a></li>
+                 <li className="product-item"><a href="#">Electronics(32)</a></li>
+                 <li className="product-item"><a href="#">Electronics(32)</a></li>
+             </ul>
+             <span className="view-item"><a href=""> View all categories<IoIosArrowForward className="arrow"/></a></span>
+         </div>
+         <div className="middle-side">
+             <div className="single-product-wrapper">
+                 
+             </div>
+         </div>
+         <div className="right-side"></div>
+     </section>
+ </main>
     );
   }
   
